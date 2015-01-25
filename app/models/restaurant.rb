56d9cb0d89b354a081cf1bc70174fbf7f27ac7e5
Kenneth Lee <kennyxlee@gmail.com>
2015-01-25 15:32:50 -0800

    Add name/addy validations to Restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ActiveRecord::Base
   has_many :suggestions
   has_many :users, through: :suggestions
+
+  validates :name, presence: true
+  validates :address, presence: true
 end
