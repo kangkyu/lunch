41f200ad6b7b8500644ecdefd866d12ef215ae98
Kenneth Lee <kennyxlee@gmail.com>
2015-01-25 08:30:49 -0800

    Login views for devise, add devise flash messages to navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true, length: { maximum: 50 }
+  # validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
   validates :password, presence: true, length: { minimum: 8 }
 end
